db4db54bbf952b8303638f6fd5edb65e10385e9a
hunt burd <hunt@websitescenes.com>
2013-08-18 11:38:26 -0600

    Rails 3 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PointOfSale::Application.config.secret_token = 'dde8931df632e895a2c82f84764d3a8f6512cc9e891863b39b58bc728c57f86cd8939fb52c4fc6942bfc19567d37da95d35778d5b4325cbe3dd97cc5ff87f97f'
