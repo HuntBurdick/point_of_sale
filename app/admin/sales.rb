7dc100feb72987cc41c8be47b693808a41b46703
hunt burd <hunt@websitescenes.com>
2013-09-02 11:36:57 -0600

    deciding how to do the cart thing.

--- app/admin/sales.rb
+++ app/admin/sales.rb
@@ -17,6 +17,15 @@ ActiveAdmin.register Sale do
 
 	end
 
+	collection_action :remove_all_product  do
+		
+		session.delete(:products)
+		respond_to do |format|
+	        # format.html 
+	        format.js 
+	      end
+	end
+
 	show do
     # renders app/views/admin/pages/_some_partial.html.erb
     render "form"
