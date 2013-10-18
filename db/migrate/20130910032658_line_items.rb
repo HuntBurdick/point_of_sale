ffb2f5172b743b476084b1d782e6f6fbc4f5e6d7
hunt burd <hunt@websitescenes.com>
2013-10-18 01:43:14 -0600

    asdfasdf

--- db/migrate/20130910032658_line_items.rb
+++ db/migrate/20130910032658_line_items.rb
@@ -1,17 +1,16 @@
 class LineItems < ActiveRecord::Migration
   def change
-    drop_table :line_items
-  	# create_table :line_items do |t|
-   #    t.integer :item_id
-   #    t.integer :quantity, :default => 1
-   #    t.integer :sale_id
-   #    t.integer :work_order_id
+  	create_table :line_items do |t|
+      t.integer :item_id
+      t.integer :quantity, :default => 1
+      t.integer :sale_id
+      t.integer :work_order_id
 
-   #    t.timestamps
-   #  end
+      t.timestamps
+    end
   end
 
   def self.down
-    drop_table :line_items
+    # drop_table :line_items
   end
 end
