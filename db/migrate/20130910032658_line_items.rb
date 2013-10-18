58fe1feb15550fbaa694d6e7cab87be0856c8552
hunt burd <hunt@websitescenes.com>
2013-10-18 01:38:21 -0600

    tricky raptor

--- db/migrate/20130910032658_line_items.rb
+++ db/migrate/20130910032658_line_items.rb
@@ -1,13 +1,13 @@
 class LineItems < ActiveRecord::Migration
   def change
-  	# create_table :line_items do |t|
-   #    t.integer :item_id
-   #    t.integer :quantity, :default => 1
-   #    t.integer :sale_id
-   #    t.integer :work_order_id
+  	create_table :line_items do |t|
+      t.integer :item_id
+      t.integer :quantity, :default => 1
+      t.integer :sale_id
+      t.integer :work_order_id
 
-   #    t.timestamps
-   #  end
+      t.timestamps
+    end
   end
 
   def self.down
